6e8ecfd68cf0fedda4e74202aa37ef8bef426f15
Alexander Song <axiomofjoy@gmail.com>
2023-06-23 08:26:02 -0700

    vertexai notebooks

--- tutorials/build_arize_docs_index_langchain_pinecone.py
+++ tutorials/build_arize_docs_index_langchain_pinecone.py
@@ -1,28 +1,26 @@
 """
-Builds an index for the Arize documentation using LangChain and Pinecone.
+Builds an index for the Arize documentation using LangChain, Pinecone, and Google's VertexAI API.
 
 To run, you must first create an account with Pinecone and create an index in the UI with the
-appropriate embedding dimension (1536 if you are using text-embedding-ada-002 like this script). You
-also need an OpenAI API key. This implementation relies on the fact that the Arize documentation is
-written and hosted with Gitbook. If your documentation does not use Gitbook, you should use a
-different document loader.
+appropriate embedding dimension (768 if you are using textembedding-gecko like this script). You
+also need a GCP account with the VertexAI API enabled. This implementation relies on the fact that
+the Arize documentation is written and hosted with Gitbook. If your documentation does not use
+Gitbook, you should use a different document loader.
 """
 
 import argparse
 import logging
 import sys
-from functools import partial
-from typing import Dict, List, Optional
+from typing import Dict, List
 
 import numpy as np
-import openai
 import pandas as pd
 import pinecone  # type: ignore
-import tiktoken
+import vertexai  # type: ignore
 from langchain.docstore.document import Document
 from langchain.document_loaders import GitbookLoader
 from langchain.embeddings.base import Embeddings
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings.vertexai import VertexAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Pinecone
 from tiktoken import Encoding
@@ -61,12 +59,16 @@ def chunk_docs(documents: List[Document], embedding_model_name: str) -> List[Doc
     - https://www.youtube.com/watch?v=eqOfr4AGLk8
     """
 
+    # text_splitter = MarkdownTextSplitter()
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=400,
-        chunk_overlap=20,
-        length_function=partial(
-            tiktoken_len, tokenizer=tiktoken.encoding_for_model(embedding_model_name)
-        ),
+        # chunk_size=400,
+        # chunk_overlap=20,
+        # length_function=partial(
+        #     tiktoken_len, tokenizer=tiktoken.get_encoding("cl100k_base"),
+        # ),
+        chunk_size=1000,
+        chunk_overlap=50,
+        length_function=len,
         separators=["\n\n", "\n", " ", ""],
     )
     return text_splitter.split_documents(documents)
@@ -90,9 +92,9 @@ def save_dataframe_to_parquet(dataframe: pd.DataFrame, save_path: str) -> None:
     dataframe.to_parquet(save_path)
 
 
-class OpenAIEmbeddingsWrapper(OpenAIEmbeddings):
+class VertexAIEmbeddingsWrapper(VertexAIEmbeddings):
     """
-    Wrapper around OpenAIEmbeddings that stores the query and document embeddings in memory.
+    Wrapper around VertexAIEmbeddings that stores the query and document embeddings in memory.
     """
 
     query_text_to_embedding: Dict[str, List[float]] = {}
@@ -103,8 +105,8 @@ class OpenAIEmbeddingsWrapper(OpenAIEmbeddings):
         self.query_text_to_embedding[text] = embedding
         return embedding
 
-    def embed_documents(self, texts: List[str], chunk_size: Optional[int] = 0) -> List[List[float]]:
-        embeddings = super().embed_documents(texts, chunk_size)
+    def embed_documents(self, texts: List[str], batch_size: int = 5) -> List[List[float]]:
+        embeddings = super().embed_documents(texts, batch_size)
         for text, embedding in zip(texts, embeddings):
             self.document_text_to_embedding[text] = embedding
         return embeddings
@@ -138,25 +140,29 @@ if __name__ == "__main__":
     parser.add_argument("--pinecone-api-key", type=str, help="Pinecone API key")
     parser.add_argument("--pinecone-index-name", type=str, help="Pinecone index name")
     parser.add_argument("--pinecone-environment", type=str, help="Pinecone environment")
-    parser.add_argument("--openai-api-key", type=str, help="OpenAI API key")
+    parser.add_argument("--gcp-project-id", type=str, help="GCP project ID")
+    parser.add_argument("--gcp-location", type=str, help="GCP location")
     parser.add_argument(
-        "--output-parquet-path", type=str, help="Path to output parquet file for index"
+        "--output-parquet-path",
+        type=str,
+        help="Path to output parquet file for index",
     )
     args = parser.parse_args()
 
     pinecone_api_key = args.pinecone_api_key
     pinecone_index_name = args.pinecone_index_name
     pinecone_environment = args.pinecone_environment
-    openai_api_key = args.openai_api_key
+    gcp_project_id = args.gcp_project_id
+    gcp_location = args.gcp_location
     output_parquet_path = args.output_parquet_path
 
-    openai.api_key = openai_api_key
+    vertexai.init(project=gcp_project_id, location=gcp_location)
     pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment)
 
     docs_url = "https://docs.arize.com/arize/"
-    embedding_model_name = "text-embedding-ada-002"
+    embedding_model_name = "textembedding-gecko"
     documents = load_gitbook_docs(docs_url)
     documents = chunk_docs(documents, embedding_model_name=embedding_model_name)
-    embeddings = OpenAIEmbeddingsWrapper(model=embedding_model_name)  # type: ignore
+    embeddings = VertexAIEmbeddingsWrapper(model=embedding_model_name)  # type: ignore
     build_pinecone_index(documents, embeddings, pinecone_index_name)
     save_dataframe_to_parquet(embeddings.document_embedding_dataframe, output_parquet_path)
